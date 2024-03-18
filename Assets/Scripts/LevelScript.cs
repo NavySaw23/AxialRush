6c0b9df0ff5ecf2e2365d263362cbab25ee0577b
Navinya <Navinya Sawarkar>
2024-03-18 23:27:07 +0530

    rotation work properly in 2 axis

--- Assets/Scripts/LevelScript.cs
+++ Assets/Scripts/LevelScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,12 @@ using UnityEngine;
 public class LevelScript : MonoBehaviour
 {
     public Manager gm;
-    private float rotationAmount = 90f; 
+    private float rotationAmount = 90f;
     private bool isRotating = false;
     private Quaternion targetRotation;
     private Vector3 currentEulerAngles;
+
+    private int turnvar = 0;
 
     void Start()
     {
@@ -22,25 +25,70 @@ public class LevelScript : MonoBehaviour
         {
             Vector3 rotationDelta = Vector3.zero;  //Initial rotation
 
-            //roll left and right
-            if (Input.GetKeyDown(KeyCode.Keypad7))
-            {
-                rotationDelta += new Vector3(0, 0, rotationAmount);
-            }
-            else if (Input.GetKeyDown(KeyCode.Keypad9))
-            {
-                rotationDelta += new Vector3(0, 0, -rotationAmount);
-            }
-
             // turn left and right
-            else if (Input.GetKeyDown(KeyCode.Keypad4))
+            if (Input.GetKeyDown(KeyCode.Keypad4))
             {
                 rotationDelta += new Vector3(0, rotationAmount, 0);
+                turnvar = (turnvar - 1 + 4) % 4;
             }
             else if (Input.GetKeyDown(KeyCode.Keypad6))
             {
                 rotationDelta += new Vector3(0, -rotationAmount, 0);
+                turnvar = (turnvar + 1) % 4;
             }
+            
+
+
+            if (turnvar == 0)
+            {
+                // roll left and right
+                if (Input.GetKeyDown(KeyCode.Keypad7))
+                {
+                    rotationDelta += new Vector3(0, 0, rotationAmount);
+                }
+                else if (Input.GetKeyDown(KeyCode.Keypad9))
+                {
+                    rotationDelta += new Vector3(0, 0, -rotationAmount);
+                }
+            }
+            else if (turnvar == 1)
+            {
+                // roll left and right
+                if (Input.GetKeyDown(KeyCode.Keypad7))
+                {
+                    rotationDelta += new Vector3(rotationAmount, 0, 0);
+                }
+                else if (Input.GetKeyDown(KeyCode.Keypad9))
+                {
+                    rotationDelta += new Vector3(-rotationAmount, 0, 0);
+                }
+            }
+            else if (turnvar == 2)
+            {
+                // roll left and right
+                if (Input.GetKeyDown(KeyCode.Keypad7))
+                {
+                    rotationDelta += new Vector3(0, 0, -rotationAmount);
+                }
+                else if (Input.GetKeyDown(KeyCode.Keypad9))
+                {
+                    rotationDelta += new Vector3(0, 0, rotationAmount);
+                }
+            }
+            else if (turnvar == 3)
+            {
+                 // roll left and right
+                if (Input.GetKeyDown(KeyCode.Keypad7))
+                {
+                    rotationDelta += new Vector3(-rotationAmount, 0, 0);
+                }
+                else if (Input.GetKeyDown(KeyCode.Keypad9))
+                {
+                    rotationDelta += new Vector3(rotationAmount, 0, 0);
+                }
+            }
+
+
 
             if (rotationDelta != Vector3.zero)
             {
@@ -62,11 +110,14 @@ public class LevelScript : MonoBehaviour
         while (t < gm.rotationCooldownSec)
         {
             t += Time.deltaTime;
-            transform.rotation = Quaternion.Lerp(startRotation, targetRotation, gm.rotationSpeed*t);
+            transform.rotation = Quaternion.Lerp(startRotation, targetRotation, gm.rotationSpeed * t);
             yield return null;
         }
 
         transform.rotation = targetRotation; // Ensure the target rotation is exactly reached
         isRotating = false;
+
+        Debug.Log(transform.rotation.eulerAngles);
+        Debug.Log(turnvar);
     }
 }
--- Assets/Scripts/LevelScriptLastWorking.cs
+++ Assets/Scripts/LevelScriptLastWorking.cs
@@ -134,3 +134,129 @@
 //         isRotating = false;
 //     }
 // }
+
+
+// ----------------------------------------------------------------------------------------------
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class LevelScript : MonoBehaviour
+// {
+//     public Manager gm;
+//     private float rotationAmount = 90f;
+//     private bool isRotating = false;
+//     private Quaternion targetRotation;
+//     private Vector3 currentEulerAngles;
+
+//     private int turnvar = 0;
+
+//     void Start()
+//     {
+//         targetRotation = transform.rotation;
+//         currentEulerAngles = transform.eulerAngles;
+//     }
+
+//     void Update()
+//     {
+//         if (!isRotating)
+//         {
+//             Vector3 rotationDelta = Vector3.zero;  //Initial rotation
+
+//             // turn left and right
+//             if (Input.GetKeyDown(KeyCode.Keypad4))
+//             {
+//                 rotationDelta += new Vector3(0, rotationAmount, 0);
+//                 turnvar = (turnvar - 1 + 4) % 4;
+//             }
+//             else if (Input.GetKeyDown(KeyCode.Keypad6))
+//             {
+//                 rotationDelta += new Vector3(0, -rotationAmount, 0);
+//                 turnvar = (turnvar + 1) % 4;
+//             }
+            
+
+
+//             if (turnvar == 0)
+//             {
+//                 // roll left and right
+//                 if (Input.GetKeyDown(KeyCode.Keypad7))
+//                 {
+//                     rotationDelta += new Vector3(0, 0, rotationAmount);
+//                 }
+//                 else if (Input.GetKeyDown(KeyCode.Keypad9))
+//                 {
+//                     rotationDelta += new Vector3(0, 0, -rotationAmount);
+//                 }
+//             }
+//             else if (turnvar == 1)
+//             {
+//                 // roll left and right
+//                 if (Input.GetKeyDown(KeyCode.Keypad7))
+//                 {
+//                     rotationDelta += new Vector3(rotationAmount, 0, 0);
+//                 }
+//                 else if (Input.GetKeyDown(KeyCode.Keypad9))
+//                 {
+//                     rotationDelta += new Vector3(-rotationAmount, 0, 0);
+//                 }
+//             }
+//             else if (turnvar == 2)
+//             {
+//                 // roll left and right
+//                 if (Input.GetKeyDown(KeyCode.Keypad7))
+//                 {
+//                     rotationDelta += new Vector3(0, 0, -rotationAmount);
+//                 }
+//                 else if (Input.GetKeyDown(KeyCode.Keypad9))
+//                 {
+//                     rotationDelta += new Vector3(0, 0, rotationAmount);
+//                 }
+//             }
+//             else if (turnvar == 3)
+//             {
+//                  // roll left and right
+//                 if (Input.GetKeyDown(KeyCode.Keypad7))
+//                 {
+//                     rotationDelta += new Vector3(-rotationAmount, 0, 0);
+//                 }
+//                 else if (Input.GetKeyDown(KeyCode.Keypad9))
+//                 {
+//                     rotationDelta += new Vector3(rotationAmount, 0, 0);
+//                 }
+//             }
+
+
+
+//             if (rotationDelta != Vector3.zero)
+//             {
+//                 currentEulerAngles += rotationDelta;
+//                 currentEulerAngles.x = Mathf.Repeat(currentEulerAngles.x, 360);
+//                 currentEulerAngles.y = Mathf.Repeat(currentEulerAngles.y, 360);
+//                 targetRotation = Quaternion.Euler(currentEulerAngles);
+//                 StartCoroutine(RotateToTarget());
+//             }
+//         }
+//     }
+
+//     IEnumerator RotateToTarget()
+//     {
+//         isRotating = true;
+//         Quaternion startRotation = transform.rotation;
+//         float t = 0f;
+
+//         while (t < gm.rotationCooldownSec)
+//         {
+//             t += Time.deltaTime;
+//             transform.rotation = Quaternion.Lerp(startRotation, targetRotation, gm.rotationSpeed * t);
+//             yield return null;
+//         }
+
+//         transform.rotation = targetRotation; // Ensure the target rotation is exactly reached
+//         isRotating = false;
+
+//         Debug.Log(transform.rotation.eulerAngles);
+//         Debug.Log(turnvar);
+//     }
+// }
