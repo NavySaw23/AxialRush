2620fc5ebb58f5b20d948f07eb4e664e18fab3b4
Navinya <Navinya Sawarkar>
2024-03-18 23:50:59 +0530

    touch and gyro inputs

--- Assets/Scripts/LevelScript.cs
+++ Assets/Scripts/LevelScript.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 
 public class LevelScript : MonoBehaviour
 {
+
     public Manager gm;
     private float rotationAmount = 90f;
     private bool isRotating = false;
     private Quaternion targetRotation;
     private Vector3 currentEulerAngles;
 
-    private int turnvar = 0;
+
+    private int yawvar = 0;
+    private int rollvar = 0;
+
+    private int lr = 0;
+    private Vector2 touchStartPos;
+    private bool isSwipe;
+
 
     void Start()
     {
@@ -21,68 +29,116 @@ public class LevelScript : MonoBehaviour
 
     void Update()
     {
+
+
+        //gyro inputs
+        Gyroscope gyro = Input.gyro;
+        gyro.enabled = true;
+
+        // default orientation of the device
+        Vector3 tilt = Input.acceleration;
+        tilt = Quaternion.Euler(90, 0, 0) * tilt;  
+        Debug.Log(tilt);
+
+        // touch inputs (swipe)
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                touchStartPos = touch.position;
+                isSwipe = true;
+            }
+            else if (touch.phase == TouchPhase.Moved && isSwipe)
+            {
+                float deltaX = touch.position.x - touchStartPos.x;
+                float deltaY = touch.position.y - touchStartPos.y;
+
+                if (Mathf.Abs(deltaX) > Mathf.Abs(deltaY))
+                {
+                    if (deltaX < 0) // Left swipe detected
+                    {
+                        lr = -1;
+                    }
+                    else if (deltaX > 0) // Right swipe detected
+                    {
+                        lr = 1;
+                    }
+                }
+
+                isSwipe = false;
+            }
+        }
+
+
+
+        //rotation phyics
+
         if (!isRotating)
         {
             Vector3 rotationDelta = Vector3.zero;  //Initial rotation
 
             // turn left and right
-            if (Input.GetKeyDown(KeyCode.Keypad4))
+            if (Input.GetKeyDown(KeyCode.Keypad4) || lr == -1)
             {
                 rotationDelta += new Vector3(0, rotationAmount, 0);
-                turnvar = (turnvar - 1 + 4) % 4;
+                yawvar = (yawvar - 1 + 4) % 4;
+                lr = 0;
             }
-            else if (Input.GetKeyDown(KeyCode.Keypad6))
+            else if (Input.GetKeyDown(KeyCode.Keypad6) || lr == 1)
             {
                 rotationDelta += new Vector3(0, -rotationAmount, 0);
-                turnvar = (turnvar + 1) % 4;
+                yawvar = (yawvar + 1) % 4;
+                lr = 0;
             }
             
 
 
-            if (turnvar == 0)
+            if (yawvar == 0)
             {
                 // roll left and right
-                if (Input.GetKeyDown(KeyCode.Keypad7))
+                if (Input.GetKeyDown(KeyCode.Keypad7)||tilt.x < -0.8f)
                 {
                     rotationDelta += new Vector3(0, 0, rotationAmount);
                 }
-                else if (Input.GetKeyDown(KeyCode.Keypad9))
+                else if (Input.GetKeyDown(KeyCode.Keypad9)||tilt.x > 0.8f)
                 {
                     rotationDelta += new Vector3(0, 0, -rotationAmount);
                 }
             }
-            else if (turnvar == 1)
+            else if (yawvar == 1)
             {
                 // roll left and right
-                if (Input.GetKeyDown(KeyCode.Keypad7))
+                if (Input.GetKeyDown(KeyCode.Keypad7)||tilt.x < -0.8f)
                 {
                     rotationDelta += new Vector3(rotationAmount, 0, 0);
                 }
-                else if (Input.GetKeyDown(KeyCode.Keypad9))
+                else if (Input.GetKeyDown(KeyCode.Keypad9)||tilt.x > 0.8f)
                 {
                     rotationDelta += new Vector3(-rotationAmount, 0, 0);
                 }
             }
-            else if (turnvar == 2)
+            else if (yawvar == 2)
             {
                 // roll left and right
-                if (Input.GetKeyDown(KeyCode.Keypad7))
+                if (Input.GetKeyDown(KeyCode.Keypad7)||tilt.x < -0.8f)
                 {
                     rotationDelta += new Vector3(0, 0, -rotationAmount);
                 }
-                else if (Input.GetKeyDown(KeyCode.Keypad9))
+                else if (Input.GetKeyDown(KeyCode.Keypad9)||tilt.x > 0.8f)
                 {
                     rotationDelta += new Vector3(0, 0, rotationAmount);
                 }
             }
-            else if (turnvar == 3)
+            else if (yawvar == 3)
             {
                  // roll left and right
-                if (Input.GetKeyDown(KeyCode.Keypad7))
+                if (Input.GetKeyDown(KeyCode.Keypad7)||tilt.x < -0.8f)
                 {
                     rotationDelta += new Vector3(-rotationAmount, 0, 0);
                 }
-                else if (Input.GetKeyDown(KeyCode.Keypad9))
+                else if (Input.GetKeyDown(KeyCode.Keypad9)||tilt.x > 0.8f)
                 {
                     rotationDelta += new Vector3(rotationAmount, 0, 0);
                 }
@@ -118,6 +174,6 @@ public class LevelScript : MonoBehaviour
         isRotating = false;
 
         Debug.Log(transform.rotation.eulerAngles);
-        Debug.Log(turnvar);
+        Debug.Log(yawvar);
     }
 }
